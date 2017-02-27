d87886fdca74cc5da5ef51846749183bda113afb
Milou <Milou@DESKTOP-R3P5NON>
2017-02-27 02:23:48 +0100

    Echanger avec la BDD sans exposer l'id

---
+++ ApplicatioWCFService/FichesClientsService.svc.cs
@@ -0,0 +1,45 @@
+﻿using ApplicatioWCFService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace FichesClientWCFService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class FichesClientsService : IFichesClientsService
+    {
+
+        //implementation methode "GetFiches() qui renvoie la collection Observable de fiches clients "
+        public ObservableCollection<FicheClient> GetFiches()
+        {
+            ObservableCollection<FicheClient> collec = null;
+
+            //connexion a la base
+            using (var entities = new FichesClientsEntities())
+            {
+
+                //recuperation de tous mes clients converti en fiche client
+                collec = new ObservableCollection<FicheClient>(entities.Clients.Select(client => new FicheClient()
+                {
+
+                    //conversion en fiche clients
+                    Nom = client.Nom,
+                    Prenom = client.Prenom,
+                    Age = client.Age.Value,
+                    Sexe = client.Sexe
+                }
+                ));
+            }
+
+        return collec;
+            /*throw new NotImplementedException();*/
+        }
+    }
+
+}
---
+++ ApplicatioWCFService/IFichesClientsService.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+
+/// <summary>
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///  INTERFACE QUI NOUS SERT A CREER LES METHODE / WEBSERVICES avec 
+/// </summary>
+namespace FichesClientWCFService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IFichesClientsService
+    {
+
+        //WebService permetant de recuperer des fiches clients
+        [OperationContract]
+        ObservableCollection<FicheClient> GetFiches();
+        
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    [DataContract]
+    public class FicheClient : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string nom;
+
+        string prenom;
+
+        string sexe;
+
+        int age;
+
+
+        [DataMember]
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {if(nom != value)
+                {
+                    nom = value;
+                    NotifyPropertyChanged();
+
+                }
+            }
+        }
+
+
+        [DataMember]
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {if(prenom!= value)
+                {
+
+                    prenom = value;
+                    NotifyPropertyChanged();
+
+                }
+                
+            }
+        }
+
+
+        [DataMember]
+        public string Sexe
+        {
+            get
+            {
+                return sexe;
+            }
+
+            set
+            {if(sexe!= value)
+                {
+                    sexe = value;
+                    NotifyPropertyChanged();
+                   
+                }
+                
+            }
+        }
+
+
+        [DataMember]
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {if(age!= value)
+                {
+                    age = value;
+                    NotifyPropertyChanged();
+
+                }
+            }
+        }
+
+          public void NotifyPropertyChanged([CallerMemberName] string str = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(str));
+            }
+        }
+        
+
+            
+       
+       }
+}
